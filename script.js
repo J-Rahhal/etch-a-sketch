2d9688857b068217725f728a94d5f741593ea2cc
dev-acc-practice <development-practice@proton.me>
2022-08-31 12:10:49 +0300

    added reset button and rainbow button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//select body
+const body = document.querySelector('body');
 //select container
 let container = document.getElementById('container');
 //create a grid container to add rows and columns
@@ -11,6 +13,37 @@ const grid_rows = document.getElementsByClassName('rows');
 //select columns
 const grid_cols = document.getElementsByClassName('cols');
 
+
+//create  button container
+const buttonsContainer = document.createElement("div");
+buttonsContainer.className = 'buttons';
+buttonsContainer.getElementsByClassName('buttons');
+
+body.appendChild(buttonsContainer);
+
+//create reset button
+const reset = document.createElement("input");
+reset.type = "button";
+reset.value = "reset";
+reset.className = 'reset';
+reset.getElementsByClassName("reset");
+reset.addEventListener('click', clear);
+
+//create rainbow button color
+const rainbow = document.createElement("input");
+rainbow.type = "button";
+rainbow.value = "rainbow";
+rainbow.className = 'rainbow';
+rainbow.getElementsByClassName('rainbow');
+rainbow.addEventListener('click',rainbowColors);
+
+//add reset button into container
+buttonsContainer.appendChild(reset);
+container.appendChild(buttonsContainer);
+
+//add rainbow button to buttons container
+buttonsContainer.appendChild(rainbow);
+container.appendChild(buttonsContainer);
 
 //create row function which will create the row divs
 function createRows(row) {
@@ -55,3 +88,23 @@ function draw() {
 
 //call function draw
 draw();
+
+
+//create function clear
+function clear() {
+    for (let i =0; i<grid_cols.length; i++) {
+        grid_cols[i].classList.remove('selected');
+        grid_cols[i].style.backgroundColor = '#FFFFFF';
+        grid_cols[i].classList.add('selected');
+    }
+}
+
+//create rainbow colors function
+function  rainbowColors(){
+    let color = Math.floor(Math.random() * 255);
+    for(let i =0; i < grid_cols.length; i++){
+        grid_cols[i].addEventListener("mouseover", ()=>{
+            grid_cols[i].style.backgroundColor = 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
+    });
+  }
+}
